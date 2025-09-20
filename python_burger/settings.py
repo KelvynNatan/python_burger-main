ab1e52188e6a3f2ce33a2cbffe97810e897db100
KelvynNatan <kelvynnatan456@gmail.com>
2025-09-20 18:49:31 -0300

    tird commit

--- python_burger/settings.py
+++ python_burger/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Para desenvolvimento - onde encontrar arquivos estáticos
+STATICFILES_DIRS = []
+
+# Para produção - onde coletar todos os arquivos estáticos
+STATIC_ROOT = BASE_DIR / 'staticfiles'
